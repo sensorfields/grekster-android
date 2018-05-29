d51d443763580c9b15c6ccba0b2436eda945f0a6
Miko Väli <mikovali@gmail.com>
2018-05-29 14:10:22 +0300

    Grek create screen

--- src/main/java/com/sensorfields/grekster/android/grek/create/GrekCreateFragment.java
+++ src/main/java/com/sensorfields/grekster/android/grek/create/GrekCreateFragment.java
@@ -1,10 +1,29 @@
 package com.sensorfields.grekster.android.grek.create;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import com.sensorfields.grekster.android.R;
 
 public final class GrekCreateFragment extends Fragment {
 
   public static GrekCreateFragment create() {
     return new GrekCreateFragment();
+  }
+
+  @Override
+  public View onCreateView(
+      @NonNull LayoutInflater inflater,
+      @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    View view = inflater.inflate(R.layout.grek_create_fragment, container, false);
+    Toolbar toolbar = view.findViewById(R.id.toolbar);
+    toolbar.inflateMenu(R.menu.grek_create_toolbar);
+    return view;
   }
 }
